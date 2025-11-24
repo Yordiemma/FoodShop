660e11daf23cfbad69a82ed76dcf1574c46800ef
vickhoff <dvickhoff@gmail.com>
2025-11-24 23:34:09 +0100

    Add file for product data

---
+++ product-data.js
@@ -0,0 +1,170 @@
+const productData = [
+    {
+      category: "Kött, fågel & chark",
+      name: "Falukorv Ring",
+      brand: "Scan",
+      description: "800 g",
+      sale: { saleMessage: "Sale!" },
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1742310146/cloud/455592/Falukorv%20Ring.jpg",
+      localProduced: true,
+      link: "https://www.coop.se/handla/varor/kott-fagel-chark/korv/falukorv/falukorv-ring-7300206787006",
+      currentPrice: 35,
+      oldPrice: 42
+    },
+    {
+      category: "Kött, fågel & chark",
+      name: "Paj Ost & skinka",
+      brand: "Felix",
+      description: "220 g, fryst",
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1722929387/cloud/376071/Paj%20Ost%20%26%20skinka.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/frys/fardigt-att-varma/paj/paj-ost-skinka-7310240079555",
+      currentPrice: 31.95
+    },
+    {
+      category: "Frys",
+      name: "Pizza X-Tra Allt Capricciosa",
+      brand: "Grandiosa",
+      description: "350 g, fryst",
+      sale: { saleMessage: "Sale!" },
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1713176338/cloud/344585/Pizza%20X-Tra%20Allt%20Capricciosa.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/frys/fardigt-att-varma/pizza/pizza-x-tra-allt-capricciosa-7310240071672",
+      currentPrice: 69,
+      oldPrice: 85
+    },
+    {
+      category: "Frys",
+      name: "Broccoli",
+      brand: "Coop",
+      description: "500 g.",
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1706524341/cloud/323177/Broccoli.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/frys/gronsaker/gronsaker/broccoli-7340191152670",
+      currentPrice: 17.5
+    },
+    {
+      category: "Dryck",
+      name: "7up Sockerfri",
+      brand: "7up",
+      description: "150 cl",
+      sale: { saleMessage: "3 for 2" },
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1694676808/cloud/290598/7up%20Sockerfri.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/dryck/lask/citron-limesmak/7up-sockerfri-7310070004680",
+      currentPrice: 50
+    },
+    {
+      category: "Dryck",
+      name: "Pommac Magnum",
+      brand: "Pommac",
+      description: "10 cl",
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1530117881/309118/Pommac%20Magnum.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/dryck/lask/ovriga-smaker/pommac-magnum-7391001000285",
+      currentPrice: 59.95
+    },
+    {
+      category: "Dryck",
+      name: "Zingo Zero Apelsin 6-pack",
+      brand: "Zingo",
+      description: "6x33 cl.",
+      photoUrl: "/mnt/data/Zingo Apelsin.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/dryck/lask/apelsinsmak/zingo-zero-apelsin-6-pack-7310072000765/",
+      currentPrice: 46.95
+    },
+    {
+      category: "Dryck",
+      name: "Pepsi Max",
+      brand: "Pepsi",
+      description: "1,5 liter",
+      sale: { saleMessage: "3 for 2" },
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1710775305/cloud/337339/Pepsi%20Max.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/dryck/lask/cola/pepsi-max-7310072000635",
+      currentPrice: 50
+    },
+    {
+      category: "Mejeri & ägg",
+      name: "Vispgrädde 36%",
+      brand: "Arla Köket",
+      description: "5 dl",
+      sale: { saleMessage: "30 kr/st" },
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1652099655/cloud/251745/Vispgr%C3%A4dde%2036%25.jpg",
+      localProduced: true,
+      link: "https://www.coop.se/handla/varor/mejeri-agg/gradde/vispgradde/vispgradde-36-7310865061935",
+      currentPrice: 30,
+      oldPrice: 36.5
+    },
+    {
+      category: "Mejeri & ägg",
+      name: "Blåmögelost Ädel 30%",
+      brand: "Kvibille",
+      description: "140 g",
+      sale: { saleMessage: "Sale!" },
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1763117136/cloud/554357/%C3%84delost.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/ost/palaggsost/mjukost/adelost-7311440016364/",
+      currentPrice: 29.5,
+      oldPrice: 37
+    },
+    {
+      category: "Mejeri & ägg",
+      name: "Grötbröd",
+      brand: "Pågen",
+      description: "780 g",
+      sale: { saleMessage: "30% off" },
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1661934051/cloud/258029/Gr%C3%B6tbr%C3%B6d.jpg",
+      localProduced: true,
+      link: "https://www.coop.se/handla/varor/morkt-grovt-brod-matbrod/grotbrod-7311070008708",
+      currentPrice: 27.90,
+      oldPrice: 34
+    },
+  
+    {
+      category: "Skafferi",
+      name: "Bakchoklad Mörk",
+      brand: "Coop",
+      description: "200 g",
+      sale: { saleMessage: "Sale!" },
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1729504903/cloud/395487/Bakchoklad%20M%C3%B6rk.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/skafferi/bakprodukter/choklad/bakchoklad-mork",
+      currentPrice: 36.95,
+      oldPrice: 45
+    },
+    {
+      category: "Skafferi",
+      name: "Bakchoklad Ljus",
+      brand: "Coop",
+      description: "200 g",
+      sale: { saleMessage: "Sale!" },
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1728970566/cloud/393961/Bakchoklad%20Ljus.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/skafferi/bakning/kakao-blockchoklad/bakchoklad-ljus-7340191149755/",
+      currentPrice: 36.95,
+      oldPrice: 45
+    },
+    {
+      category: "Skafferi",
+      name: "Hasselnötter",
+      brand: "Coop",
+      description: "200 g",
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1707736570/cloud/326634/Hasseln%C3%B6tter.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/fardigmat-mellanmal/mellanmal/notter/hasselnotter-7340191148468/",
+      currentPrice: 34.5,
+      oldPrice: 42
+    },
+    {
+      category: "Skafferi",
+      name: "Granola Hasselnötter & dadlar",
+      brand: "Paulúns",
+      description: "450 g",
+      photoUrl: "https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_1000,w_1000/v1617885984/426809/Granola%20Hasseln%C3%B6tter%20%26%20dadlar.jpg",
+      localProduced: false,
+      link: "https://www.coop.se/handla/varor/skafferi/flingor-musli/granola-crunch/granola-hasselnotter-dadlar-7310240001761/",
+      currentPrice: 55.95
+    }
+  ];
