101fb9eb275cea078b57841e4adb551ccc1e9003
vickhoff <dvickhoff@gmail.com>
2025-11-29 13:41:40 +0100

    Logic to cart.js

--- src/scripts/fetching.js
+++ src/scripts/fetching.js
@@ -3,7 +3,7 @@ import { addToCart } from './cart'
 import { removeFromCart } from './cart'  
 
 const listContainerOftenBought = document.getElementById('often-bought-products')
-const oftenBoughtArray = productData.slice(4, 8)
+const oftenBoughtArray = productData.slice(4, 16)
 
 const createlistOfProducts = (listContainer, productArray, verticalOrHorizontal) => {
 
