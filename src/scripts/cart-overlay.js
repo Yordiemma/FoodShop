bf373760b92552529d19c2822cad987d2f85cd95
Julia B <julia.bohlin@hyperisland.se>
2025-12-01 09:48:15 +0100

    match figma design

--- src/scripts/cart-overlay.js
+++ src/scripts/cart-overlay.js
@@ -1,29 +1,27 @@
 const cartEl = document.querySelector('.cart-overlay')
 const cartButtonEl = document.querySelector('.cart-wrap')
+const closeButtonEl = document.querySelector('.top-wrap').lastElementChild
 
 let shown = false;
 
 const cartShow = () => {
-    cartEl.style.display = 'block'
-    document.body.style.overflow = 'hidden'
-console.log(cartButtonEl.firstElementChild)
-
-    shown = true;
+    if (shown) {
+    } else {
+        cartEl.style.display = 'block'
+        document.body.style.overflow = 'hidden'
+        cartButtonEl.innerHTML = `<img src="/src/images/cart-view.svg" alt="">
+          <span>0</span>`
+        shown = true;
+    }
 }
 
 const cartHide = () => {
     cartEl.style.display = 'none'
     document.body.style.overflow = 'auto'
+    cartButtonEl.innerHTML = `<img src="/src/images/cart.svg" alt="">
+          <span>0</span>`
     shown = false;
 }
 
-const cartFunctionality = () => {
-    if (shown) {
-        cartHide();
-    } else {
-        cartShow();
-    }
-}
-
-cartButtonEl.addEventListener('click', cartFunctionality)
-
+cartButtonEl.addEventListener('click', cartShow)
+closeButtonEl.addEventListener('click', cartHide)
