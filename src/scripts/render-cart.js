2a7b02a90cec85f33140bb4f75fe5a856857dd28
Julia B <julia.bohlin@hyperisland.se>
2025-12-03 13:04:51 +0100

    remove unnecessary code in render cart

--- src/scripts/render-cart.js
+++ src/scripts/render-cart.js
@@ -1,14 +1,1 @@
-// import { cartItems } from "./cart";
-// import { totalInCart } from "./cart";
-
 export const totalItemsEl = document.querySelectorAll('.total-items')
-// export const totalPriceEl = document.getElementById('total-price')
-
-// const renderTotalItems = () => {
-//     totalInCart();
-//     totalItemsEl.textContent = totalInCart
-// }
-
-// // console.log(cartItems)
-// renderTotalItems();
-// // totalItemsEl.textContent = ;
