17fdc3083a0ec019a1aa2d4c37b2c7a0ed2eeea8
vickhoff <dvickhoff@gmail.com>
2025-12-04 00:36:46 +0100

    Correct indentation

--- src/scripts/render-often-bought.js
+++ src/scripts/render-often-bought.js
@@ -1,13 +1,16 @@
 import { productData } from "./product-data.js";
+import { renderProductList } from "./render-product-list";
 
 //Create array for often bought products
 const oftenBoughtArray = productData.slice(4, 10);
 
+//Grab list container ID
+const listContainerOftenBought = document.getElementById("often-bought-products");
+
 //Render items when loaded
 document.addEventListener("DOMContentLoaded", () => {
-  const listContainerOftenBought = document.getElementById("often-bought-products");
   if (listContainerOftenBought) {
-    createlistOfProducts(
+    renderProductList(
       listContainerOftenBought,
       oftenBoughtArray,
       "vertical"
