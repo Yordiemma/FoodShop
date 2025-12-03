12746f6643bda3ff6236560467d8bbfa45eee5c5
Julia B <julia.bohlin@hyperisland.se>
2025-12-03 13:52:17 +0100

    remove changes in cart

--- src/scripts/cart.js
+++ src/scripts/cart.js
@@ -1,5 +1,7 @@
 import { productData } from "./product-data"
 
+
+
 export let cartItems = []
 
 function containsObject(cartItem) {
@@ -14,15 +16,11 @@ function containsObject(cartItem) {
 
 export const totalInCart = () => {
     const amountContainer = document.getElementById("total-in-cart")
-    const totalItemsEl = document.querySelectorAll('.total-items')
 
     const totalInCart = cartItems.reduce((total, product) => total + product.amount, 0)
     if (totalInCart > 0) {
         amountContainer.style.opacity = 1;
         amountContainer.textContent = totalInCart;
-        totalItemsEl.forEach((span) => {
-            span.textContent = totalInCart;
-        })
     } else if (totalInCart === 0) {
         amountContainer.style.opacity = 0;
     }
@@ -30,8 +28,8 @@ export const totalInCart = () => {
 
 export const addToCart = (id, event) => {
     for (const product of productData) {
-        if (id === product.id) {
-            if (containsObject(product)) {
+        if(id === product.id) {
+            if(containsObject(product)) {
                 const cartItem = cartItems.find(item => item.id === product.id);
                 if (cartItem) {
                     cartItem.amount += 1;
@@ -60,13 +58,13 @@ export const addToCart = (id, event) => {
             const compactButton = productItem.querySelector(".compact-button")
 
             if (totalInCart)
-                if (stepper) {
-                    stepper.style.display = "flex"
-                    stepper.style.opacity = 1
+            if (stepper){
+                stepper.style.display = "flex"
+                stepper.style.opacity = 1
 
-                    compactButton.style.opacity = "0"
-                    compactButton.style.display = "none"
-                }
+                compactButton.style.opacity = "0"
+                compactButton.style.display = "none"
+            }
             totalInCart()
         }
     }
@@ -83,12 +81,12 @@ export const removeFromCart = (id, event) => {
     let productCount = 1;
 
     for (const product of productData) {
-        if (id === product.id) {
-            if (containsObject(product)) {
+        if(id === product.id) {
+            if(containsObject(product)) {
                 const cartItem = cartItems.find(item => item.id === product.id);
                 if (cartItem.amount === 1) {
                     cartItem.amount -= 1;
-                    if (stepper) {
+                    if (stepper){
                         stepper.style.opacity = 0
                         stepper.style.display = "none"
 
--- src/scripts/render-cart.js
+++ src/scripts/render-cart.js
@@ -1,12 +1,6 @@
 import { cartItems } from "./cart";
 
-export const totalItemsEl = document.querySelectorAll('.total-items')
 const totalPriceEl = document.getElementById('total-price')
 
-totalPriceEl.textContent = 10
 
-const totalPrice = () => {
-    
-}
 
-// console.log(cartItems)
