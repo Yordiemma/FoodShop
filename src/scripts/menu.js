615f3a6b1102ab6bbad2cf11a57550dce9acf9b0
foloram <ghazali.ajdar78@gmail.com>
2025-12-05 15:13:05 +0100

    create a dynamic categories in the menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,5 @@
+import { productData } from "/src/scripts/product-data.js";
+
 const menuNav = document.querySelector(".menu-nav");
 const menuContainer = document.querySelector(".menu-container");
 const menuSpan = document.querySelector(".menu-span");
@@ -13,4 +15,30 @@ menuSpan.addEventListener("click", (event) => {
   event.preventDefault();
   menuContainer.classList.remove("active");
   menuNav.classList.remove("active");
+});
+
+// Optional: Dynamically update product counts in menu buttons
+
+const categories = productData.reduce((acc, product) => {
+  acc[product.category] = (acc[product.category] || 0) + 1;
+  return acc;
+}, {});
+
+const categoriesContainer = document.querySelector(".dynamic-categories");
+
+categoriesContainer.innerHTML = "";
+
+Object.entries(categories).forEach(([catName, count]) => {
+  const item = document.createElement("div");
+  item.classList.add("menu-item", "title");
+
+  item.innerHTML = `
+    <h3>${catName}</h3>
+    <button class="menu-button">${count}</button>
+  `;
+
+  const hr = document.createElement("hr");
+
+  categoriesContainer.appendChild(item);
+  categoriesContainer.appendChild(hr);
 });
